2cb5eea9c84f3af7373d753e7054889cad804b99
Andrew Lund <andrew.s.lund@gmail.com>
2023-03-29 15:34:22 -0700

    implemented stack and queue in the es6 instantiation pattern; all tests passing

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,37 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  enqueue(value) {
+    var maxKey = 0;
+    for (var key in this.storage) {
+      if (key > maxKey) {
+        maxKey = key;
+      }
+    }
+    this.storage[maxKey + 1] = value;
+  }
+
+  dequeue() {
+    var minKey = Infinity;
+    for (var key in this.storage) {
+      if (key < minKey) {
+        minKey = key;
+      }
+    }
+    var dequeueVal = this.storage[minKey];
+    delete this.storage[minKey];
+    return dequeueVal;
+  }
+
+  size() {
+    var objSize = 0;
+    for (var key in this.storage) {
+      objSize++;
+    }
+    return objSize;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,37 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  push(value) {
+    var maxKey = 0;
+    for (var key in this.storage) {
+      if (key > maxKey) {
+        maxKey = key;
+      }
+    }
+    this.storage[maxKey + 1] = value;
+  }
+
+  pop() {
+    var maxKey = 0;
+    for (var key in this.storage) {
+      if (key > maxKey) {
+        maxKey = key;
+      }
+    }
+    var popVal = this.storage[maxKey];
+    delete this.storage[maxKey];
+    return popVal;
+  }
+
+  size() {
+    var objSize = 0;
+    for (var key in this.storage) {
+      objSize++;
+    }
+    return objSize;
   }
 
 }
