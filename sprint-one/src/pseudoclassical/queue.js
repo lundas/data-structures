b37422faee7197069c3b3c1f4ca0b93a31d183ed
Andrew Lund <andrew.s.lund@gmail.com>
2023-03-29 15:25:48 -0700

    implemented queue and stack in the Pseudoclassical Instantiation pattern; all tests passing

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  var maxKey = 0;
+  for (var key in this.storage) {
+    if (key > maxKey) {
+      maxKey = key;
+    }
+  }
+  this.storage[maxKey + 1] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  var minKey = Infinity;
+  for (var key in this.storage) {
+    if (key < minKey) {
+      minKey = key;
+    }
+  }
+  var dequeueVal = this.storage[minKey];
+  delete this.storage[minKey];
+  return dequeueVal;
+
+};
+
+Queue.prototype.size = function() {
+  var objSize = 0;
+  for (var key in this.storage) {
+    objSize++;
+  }
+  return objSize;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  var maxKey = 0;
+  for (var key in this.storage) {
+    if (key > maxKey) {
+      maxKey = key;
+    }
+  }
+  this.storage[maxKey + 1] = value;
+};
+
+Stack.prototype.pop = function () {
+  var maxKey = 0;
+  for (var key in this.storage) {
+    if (key > maxKey) {
+      maxKey = key;
+    }
+  }
+  var popVal = this.storage[maxKey];
+  delete this.storage[maxKey];
+  return popVal;
+};
+
+Stack.prototype.size = function() {
+  var objSize = 0;
+  for (var key in this.storage) {
+    objSize++;
+  }
+  return objSize;
+};
 
