4bb3e50c392e7dbf8ec8e7c445fb03b9c1be9b0e
Andrew Lund <andrew.s.lund@gmail.com>
2023-03-29 12:16:44 -0700

    implemented queue in the Functional Instantiation Style; all tests pass:wq

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,38 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  someInstance.storage = storage;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var maxKey = 0;
+    for (var key in someInstance.storage) {
+      if (key > maxKey) {
+        maxKey = key;
+      }
+    }
+    someInstance.storage[maxKey + 1] = value;
   };
 
   someInstance.dequeue = function() {
+    var minKey = Infinity;
+    for (var key in someInstance.storage) {
+      if (key < minKey) {
+        minKey = key;
+      }
+    }
+    var dequeueVal = someInstance.storage[minKey];
+    delete someInstance.storage[minKey];
+    return dequeueVal;
   };
 
   someInstance.size = function() {
+    var objSize = 0;
+    for (var key in someInstance.storage) {
+      objSize++;
+    }
+    return objSize;
   };
 
   return someInstance;
