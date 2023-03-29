2958772cf362a32d99df390b377f4d90377d2e65
Andrew Lund <andrew.s.lund@gmail.com>
2023-03-29 14:50:11 -0700

    implemented stack and queue in the Prototypal Instantiation pattern; all tests passing

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,43 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  var storage = {};
+  someInstance.storage = storage;
+
+  return someInstance;
+
 };
 
 var queueMethods = {};
+queueMethods.enqueue = function(value) {
+  var maxKey = 0;
+  for (var key in this.storage) {
+    if (key > maxKey) {
+      maxKey = key;
+    }
+  }
+  this.storage[maxKey + 1] = value;
+};
+
+queueMethods.dequeue = function() {
+  var minKey = Infinity;
+  for (var key in this.storage) {
+    if (key < minKey) {
+      minKey = key;
+    }
+  }
+  var dequeueVal = this.storage[minKey];
+  delete this.storage[minKey];
+  return dequeueVal;
+};
+
+queueMethods.size = function() {
+  var objSize = 0;
+  for (key in this.storage) {
+    objSize++;
+  }
+  return objSize;
+};
 
 
