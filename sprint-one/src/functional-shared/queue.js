876234d89db25fcf26bf9cc0a8110e55e472e9ce
Andrew Lund <andrew.s.lund@gmail.com>
2023-03-29 14:24:54 -0700

    implemented queue in the Functional-Shared Instantiation pattern; all tests passing

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,44 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  var storage = {};
+  someInstance.storage = storage;
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
 var queueMethods = {};
+queueMethods.enqueue = function(value) {
+  var maxKey = 0;
+  for (var key in this.storage) {
+    if (key > maxKey) {
+      maxKey = key;
+    }
+  }
+  this.storage[maxKey + 1] = value;
+};
+
+queueMethods.dequeue = function() {
+  var minKey = Infinity;
+  for (var key in this.storage) {
+    if (key < minKey) {
+      minKey = key;
+    }
+  }
+  var dequeueVal = this.storage[minKey];
+  delete this.storage[minKey];
+  return dequeueVal;
+};
+
+queueMethods.size = function() {
+  var objSize = 0;
+  for (key in this.storage) {
+    objSize++;
+  }
+  return objSize;
+};
 
 
