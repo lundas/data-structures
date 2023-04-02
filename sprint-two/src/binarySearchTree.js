1ca24f44ee5afc32668d8c1ef0d912a4ee2294c7
Andrew Lund <andrew.s.lund@gmail.com>
2023-04-01 18:09:17 -0700

    bst performance and find closest number

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -13,8 +13,7 @@ var BinarySearchTree = function(value) {
 BinarySearchTree.prototype.insert = function(value) {
   var isGreater = value > this.value;
   if (this.value === value) {
-    // throw new Error('Value already in tree');
-    throw new Error('Value already in tree');
+  // throw new Error('Value already in tree');
   }
 
   if (!this.right && isGreater) {
@@ -59,3 +58,92 @@ BinarySearchTree.prototype.depthFirstLog = function(callback) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+var render = function(n, max) {
+  // n = number of nodes, max = maximum value for each node
+  var initialVal = Math.floor(Math.random() * max);
+  var obj = {
+    'tree': BinarySearchTree(initialVal),
+    'arr': [initialVal]//random
+  };
+
+  for (var i = 0; i < n; i++) {
+    var randomVal = Math.floor(Math.random() * max);
+    obj.tree.insert(randomVal);
+    obj.arr.push(randomVal);
+  }
+
+  return obj;
+};
+
+////// tree and array generation
+// function ( n, max){ // n has to be smaller than max. n is the number we generate.
+// for (let i =0 ; i < n; i++)
+//  floor random * max
+// tree.insert random
+// array.push random
+// }
+
+
+////// tree and array performance comparison
+// function(tree,array) { }
+// generate a random number
+// tree.contains (random number)
+// array.includes(random number)
+
+/// find closes value to tree
+  // if target >= tree.value
+  // then compare target with tree.value and tree.right
+  // if tree.right is closer-
+  // return recursively call on tree.right
+  // otherwise same on the left
+
+  // return if value = target- that's always the closest;
+  // if the parent is closer than the child, that's the closest.
+  // or if there is no child.
+
+
+var closestArr = function(arr, int) {
+  let closest = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    let currentDif = arr[i] > int ? arr[i] - int : int - arr[i];
+    let closestDif = closest > int ? closest - int : int - closest;
+    // Math.abs()
+    closest = currentDif > closestDif ? closest : arr[i];
+  }
+  return closest;
+};
+
+var closestTree = function(tree, int) {
+  // store and pass our closest value so far
+  if (int >= tree.value) {
+    if (tree.right) { // is this a parent or child in the direction.
+      let subtreeClosest = closestTree(tree.right, int);
+      return Math.abs(tree.value - int) > Math.abs(subtreeClosest - int) ? subtreeClosest : tree.value;
+    } else { // it's just a child.
+      return tree.value;
+    }
+  } else if (int < tree.value) {
+    // if int < value - recurse against left
+    if (tree.left) {
+      let subtreeClosest = closestTree(tree.left, int);
+      return Math.abs(tree.value - int) > Math.abs(subtreeClosest - int) ? subtreeClosest : tree.value;
+    } else {
+      return tree.value;
+    }
+  }
+  // if int = value, closest equals value and return
+};
+
+var compareSpeed = function(rendered, max) {
+  var randomVal = Math.floor(Math.random() * max);
+  closestTree(rendered.tree, randomVal);
+  closestArr(rendered.arr, randomVal);
+};
+
+
+/// closestTree rethink
+// if target > this.right) or target<this.left
+// recurse (this.right) or this.left
+// if target >value but < right -
+// store closest
+// return closer one between closest and recurse of right.
